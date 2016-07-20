8c392e43ca8a82a2e988a5eaeff62948f2872ecd
Justin Cletus <justinbeckh@gmail.com>
2016-07-20 12:57:19 +0530

    initial commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Task;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,7 +11,35 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::group(['middleware' => ['web']], function () {
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('tasks',[
+      'tasks' => Task::orderBy('created_at','asc')->get()
+
+    ]);
+});
+
+Route::post('/task',function(Request $request){
+  $validator = Validator::make($request->all(),[
+    'name' => 'required|max:255',
+  ]);
+  if($validator->fails()){
+    return redirect('/')
+    ->withInput()
+    ->withErrors($validator);
+  }
+  $task = new Task;
+  $task->name=$request->name;
+  $task->save();
+  return redirect('/');
+
+});
+
+
+Route::delete('/task/{id}',function($id){
+Task::findOrFail($id)->delete();
+return redirect('/');
+});
 });
